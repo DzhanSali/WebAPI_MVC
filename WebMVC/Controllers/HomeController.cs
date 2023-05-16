2373111359e72bbfc748fd90fc8da324622a28d4
DzhanSali <dzhan.erd.sali@gmail.com>
2023-05-17 00:33:37 +0300

    added sign up/in

--- WebMVC/Controllers/HomeController.cs
+++ WebMVC/Controllers/HomeController.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Text;
+using System.Text.Json;
 using WebMVC.Models;
+using WebMVC.Static_Vars;
 
 namespace WebMVC.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+
+        private IEnumerable<PersonModel_MVC> GetUsersFromAPI()
+        {
+            HttpResponseMessage response = StatClient.WebAPIClient.GetAsync("Person").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return response.Content.ReadAsAsync<IEnumerable<PersonModel_MVC>>().Result;
+            }
+            else
+            {
+                return Enumerable.Empty<PersonModel_MVC>();
+            }
+        }
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -16,6 +32,72 @@ namespace WebMVC.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginVM model)
+        {
+
+            if (!this.ModelState.IsValid)
+                return View(model);
+
+            IEnumerable<PersonModel_MVC> personList = GetUsersFromAPI();
+            bool userExists = personList.Any(p => p.Name == model.Name && p.Password == model.Password);
+
+            if (userExists)
+            {
+                return RedirectToAction("Index", "PersonController_MVC");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Invalid username or password");
+                return View(model);
+            }
+        }
+
+        public IActionResult Registration()
+        {
+
+            return View();
+            
+        }
+
+        [HttpPost]
+        public IActionResult Registration(RegistrationVM model)
+        {
+            if (!ModelState.IsValid) 
+            { 
+                return View(model); 
+            }
+            else
+            {
+                var json = JsonSerializer.Serialize(model);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = StatClient.WebAPIClient.BaseAddress;
+
+                    // Send the POST request to the API
+                    var response = client.PostAsync("Person", content).Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("Login", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Failed to register user. Please try again.");
+                        return View(model);
+                    }
+                }
+            }
+
         }
 
         public IActionResult Privacy()
---
+++ WebMVC/Models/RegistrationVM.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebMVC.Models
+{
+    public class RegistrationVM
+    {
+        [Required]
+        [StringLength(100)]
+        public string Password { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+        [Required]
+        public short Age { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Address { get; set; }
+        public char Gender { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
